2d3120994080fe76c3e222ee7cd089c17868962d
liushihua <yingtian@163.com>
2024-08-06 16:07:57 +0800

    调整

--- app/src/main/java/com/exa/companydemo/test/TabFm.kt
+++ app/src/main/java/com/exa/companydemo/test/TabFm.kt
@@ -4,30 +4,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.exa.baselib.base.BaseFragment
+import com.exa.baselib.utils.L
 import com.exa.companydemo.R
-import com.exa.companydemo.utils.WebViewConfig
 
 /**
  * @Author lsh
  * @Date 2023/8/2 18:22
  * @Description
  */
-class TabFm(var title:String) : BaseFragment() {
-    private lateinit var webView:WebView
-    private lateinit var rootView:LinearLayout
+class TabFm(var title: String) : BaseFragment() {
+    private lateinit var rootView: FrameLayout
+    private var dialog: DemoDialog? = null
     override fun setContentView(inflater: LayoutInflater, container: ViewGroup?): View {
         return inflater.inflate(R.layout.fragment_tab, container, false)
     }
 
     override fun initView(view: View) {
         view.findViewById<TextView>(R.id.tv).text = title
-        rootView= view.findViewById(R.id.rootView)
+        rootView = view.findViewById(R.id.rootView)
+        view.findViewById<TextView>(R.id.btn).setOnClickListener {
+            L.d("$title dialog?.show()")
+            dialog ?: run { dialog = DemoDialog(requireContext()) }
+            dialog?.show(childFragmentManager)
+        }
     }
 
     override fun initData() {
 
+    }
+
+    override fun onStop() {
+        super.onStop()
+        L.dd()
+        dialog?.dismiss()
     }
 }
